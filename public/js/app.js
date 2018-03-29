5440325b3e4ff98c82f53f1b765cd743fb96f9b2
Jonathan <jnflion@gmail.com>
2018-03-29 11:22:41 +0200

    Fin de l'exercice 4 : transfert d'enfants

--- public/js/app.js
+++ public/js/app.js
@@ -89,3 +89,31 @@ navButtons.forEach((element) => {
 navButtons.forEach((element) => {
   element.addEventListener("mouseout", changeTitle2)
 });
+
+// Exo3
+
+
+// Exo4
+// Récupération de mon bouton
+let boutondeplacement = document.querySelector("#exo4 button");
+
+// Récupération des div parents et enfants
+let parent1 = document.querySelector("#exo4 #parent1");
+
+let parent2 = document.querySelector("#exo4 #parent2");
+
+let enfant = document.querySelector("#exo4 #enfant")
+
+// Fonction de déplacement
+let deplacement = function () {
+
+  if (parent1.children.length != 0) {
+    // Mettre le le contenu de ma col dans l'autre
+    parent2.appendChild(enfant);
+  } else {
+    parent1.appendChild(enfant);
+  }
+};
+console.log(parent2.children);
+boutondeplacement.addEventListener("click", deplacement);
+
