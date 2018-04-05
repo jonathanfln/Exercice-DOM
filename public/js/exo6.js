e8d72af5cd0720fd6040fb38615d419504fd8e93
Jonathan <jnflion@gmail.com>
2018-04-05 13:59:00 +0200

    Calculatrice finie

--- public/js/exo6.js
+++ public/js/exo6.js
@@ -24,10 +24,14 @@ let btnCalc = document.querySelectorAll("#niv2 .chiffre");
 let btnOperation = document.querySelectorAll("#niv2 .btnOperation");
 // Récupération du bouton égal
 let egal = document.querySelector("#niv2 #btnEgal");
+// Récupérer le bouton clear
+let btnClear = document.querySelector("#niv2 #btnC");
 // Récupération de mon afficheur
 let spanAffiche = document.querySelector("#niv2 .spanAffiche");
 // Récupération de l'input
 let input = document.querySelector("#niv2 .spanEcrit");
+// total input
+let totalInput;
 // Déclarer le nombre
 let nombre;
 // Déclarer l'opérateur
@@ -46,18 +50,67 @@ let addNumber = function (event) {
 let chooseOperator = function (event) {
   // Récupérer la valeur de l'input
   nombre = input.value;
+
   // On récupère l'opérateur
   operateur = event.target.textContent;
-  // On place le nombre et l'opérateur dans le résultat
-  spanAffiche.textContent = nombre + operateur;
-  // Vider l'input
-  input.value = "";
+
+  // Vérifier si le nombre est vide
+  if (spanAffiche.textContent == "" && input.value == "") {
+    // On affiche un message d'erreur
+    spanAffiche.textContent = "Choose number";
+    // Mettre une bordure rouge sur l'input
+    input.classList.add("border", "border-danger");
+    // Enlever le border-0
+    input.classList.remove("border-0");
+    return;
+  }
+
+  // Vérifier si nous n'avons pas déjà un opérateur 
+  if (spanAffiche.textContent.match(/[\+\/\-\*]/)) {
+    spanAffiche.textContent = spanAffiche.textContent.replace(/[\+\/\-\*]/, event.target.textContent);
+    return;
+  };
+
+  // Si il y a déjà du contenu sans le span affiche, il le réutilise
+  if (spanAffiche.textContent != "" && input.value == "") {
+    spanAffiche.textContent += operateur;
+    totalInput = spanAffiche.textContent;
+  }
+
+  if (spanAffiche.textContent == "" && input.value != "") {
+    // Retirer la border rouge
+    input.classList.remove("border", "border-danger");
+
+    // remettre le border-0
+    input.classList.add("border-0");
+
+    // On place le nombre et l'opérateur dans le résultat
+    spanAffiche.textContent = input.value + operateur;
+
+    // récupération de l'input avant de le vider
+    totalInput = spanAffiche.textContent;
+
+    // Vider l'input
+    input.value = "";
+  }
+
+  if (spanAffiche.textContent != "" && input.value != "") {
+    // On place le nombre et l'opérateur dans le résultat
+    spanAffiche.textContent = input.value + operateur;
+
+    // récupération de l'input avant de le vider
+    totalInput = spanAffiche.textContent;
+
+    // Vider l'input
+    input.value = "";
+  }
+
 };
 
 // Faire le calcul
 let calculate = function (event) {
   // Récupérer les deux nombre à calculer
-  let num1 = parseFloat(nombre);
+  let num1 = parseFloat(totalInput);
   let num2 = parseFloat(input.value);
   let numResult;
 
@@ -81,6 +134,11 @@ let calculate = function (event) {
   input.value = "";
 };
 
+let clear = function () {
+  input.value = "";
+  spanAffiche.textContent = "";
+};
+
 
 // Ajouter le clic au chiffres
 btnCalc.forEach((element) => {
@@ -90,5 +148,8 @@ btnCalc.forEach((element) => {
 btnOperation.forEach((element) => {
   element.addEventListener("click", chooseOperator)
 });
-// Ajouter un eventListener addition
+// Ajouter un eventListener pour égal
 egal.addEventListener("click", calculate);
+
+// Ajouter un eventListener pour clear
+btnClear.addEventListener("click", clear);
